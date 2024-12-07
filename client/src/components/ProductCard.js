75d81efb74769a55c514d9564d3d40d53351f692
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-12-06 23:04:47 -0300

    añadir a carrito eliminado en productos

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const addToCart = (product, updateCartCount) => {
-    const cart = JSON.parse(localStorage.getItem('cart')) || [];
-    const productExists = cart.find(item => item.id === product.id);
-
-    if (!productExists) {
-        cart.push(product);
-        localStorage.setItem('cart', JSON.stringify(cart));
-        alert('Producto agregado al carrito');
-        
-        // Actualizamos el contador del carrito
-        if (updateCartCount) {
-            updateCartCount(cart.length); // Calculamos el nuevo tamaño del carrito
-        }
-    } else {
-        alert('Este producto ya está en el carrito');
-    }
-};
-
-const ProductCard = ({ product, updateCartCount, isFeatured }) => {
+const ProductCard = ({ product, isFeatured }) => {
     const isLongTitle = product.title.split(' ').length > 10; // Para títulos largos
 
     return (
@@ -29,10 +11,6 @@ const ProductCard = ({ product, updateCartCount, isFeatured }) => {
                 <h2 className={isLongTitle ? 'overflow-title' : ''}>{product.title}</h2>
                 <p>${product.price.toFixed(2)}</p>
             </Link>
-            {/* Solo mostrar el botón si el producto no es destacado */}
-            {!isFeatured && (
-                <button onClick={() => addToCart(product, updateCartCount)}>Añadir al carrito</button>
-            )}
         </div>
     );
 };
